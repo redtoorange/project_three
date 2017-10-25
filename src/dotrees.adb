fe5060b2b407f3876bb954f31e2f48781024ec26
redtoorange <redtoorange@gmail.com>
2017-10-25 14:45:30 -0400

    Removed the test comment

--- src/dotrees.adb
+++ src/dotrees.adb
@@ -58,7 +58,7 @@
 --  TREE: 3333333   1:
 --      3.0       3.0       3.0
 --      0.0       0.0       0.0
--- Some minor change to test git
+
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Command_Line; use Ada.Command_Line;
 with Ada.Directories; use Ada.Directories;
