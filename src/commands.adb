6e35d80c9433baee33ef9d309f06a63e5f15c075
redtoorange <redtoorange@gmail.com>
2017-10-26 07:53:04 -0400

    Added minor formatting tweaks

--- src/commands.adb
+++ src/commands.adb
@@ -10,34 +10,32 @@
 --    package.
 
 package body Commands is
-   procedure Trees_Command(    tl : in Tree_List );
-   procedure Fruits_Command(   tl : in Tree_List );
-   procedure Averages_Command( tl : in Tree_List );
-
+   procedure Trees_Command (tl : in Tree_List);
+   procedure Fruits_Command (tl : in Tree_List);
+   procedure Averages_Command (tl : in Tree_List);
 
    ----------------------------------------------------------------------------
    -- Process_Commands --------------------------------------------------------
    ----------------------------------------------------------------------------
-   procedure Process_Commands( tl : in Tree_List)
-   is
+   procedure Process_Commands (tl : in Tree_List) is
       -- Custom input package to handle our command input.
-      package Command_IO  is new Enumeration_IO(Command);
+      package Command_IO is new Enumeration_IO (Command);
       use Command_IO;
       c_command : Command;
    begin
       -- Continue looping until the user enters "QUIT" or we hit an EoF.
       loop
          begin
-            Get(c_command);
+            Get (c_command);
 
             -- Execute a command based on our input
             case c_command is
                when TREES =>
-                  Trees_Command(tl);
+                  Trees_Command (tl);
                when FRUITS =>
-                  Fruits_Command(tl);
+                  Fruits_Command (tl);
                when AVERAGES =>
-                  Averages_Command(tl);
+                  Averages_Command (tl);
                when QUIT =>
                   return;
             end case;
@@ -45,91 +43,83 @@ package body Commands is
          exception
             when End_Error =>
                -- Would typically happen if a command file was piped in.
-               Put_Line("End of File Reached.");
+               Put_Line ("End of File Reached.");
                return;
 
             when Data_Error =>
                -- User put in a bad command, trash it and move on.
-               Put_Line("Invalid Command.");
+               Put_Line ("Invalid Command.");
                Skip_Line;
 
             when others =>
                -- This is probably fatal, just leave.
-               Put_Line("An unknown error has occurred, exiting...");
+               Put_Line ("An unknown error has occurred, exiting...");
                return;
          end;
       end loop;
    end Process_Commands;
 
-
-   --########################################################################--
+    --########################################################################--
    ----------------------- Private Subroutines --------------------------------
-   --########################################################################--
-
+    --########################################################################--
 
    ----------------------------------------------------------
    -- Purpose: Print all the Trees in the Tree_List along with
    --    their Fruit count.
    -- Parameters: tl: Tree_List to print
    ----------------------------------------------------------
-   procedure Trees_Command( tl : in Tree_List )
-   is
+   procedure Trees_Command (tl : in Tree_List) is
    begin
       -- For each Tree in the Tree_List, print out the ID and Fruit count.
       for t in 1 .. tl.count loop
-         Put_Tree( tl.trees(t) );
+         Put_Tree (tl.trees (t));
          New_Line;
       end loop;
    end Trees_Command;
-
 
    ----------------------------------------------------------
    -- Purpose: Print all the Fruits of all Trees in the
    --    Tree_List along with their Fruit count.
    -- Parameters: tl: Tree_List to print
    ----------------------------------------------------------
-   procedure Fruits_Command( tl : in Tree_List )
-   is
+   procedure Fruits_Command (tl : in Tree_List) is
    begin
       for t in 1 .. tl.count loop
          -- Print the Tree, ID and Fruit Count
-         Put_Tree( tl.trees(t) );
-         Put(":");
+         Put_Tree (tl.trees (t));
+         Put (":");
          New_Line;
 
          -- Print all the Fruit attached to the current Tree
-         for f in 1 .. tl.trees(t).f_count loop
-            Put_Fruit( tl.trees(t).fruits(f) );
+         for f in 1 .. tl.trees (t).f_count loop
+            Put_Fruit (tl.trees (t).fruits (f));
             New_Line;
          end loop;
 
          -- Print the statistics of the Fruit for the current Tree
-         Put_Tree_Stats( tl.trees(t).stats );
+         Put_Tree_Stats (tl.trees (t).stats);
          New_Line;
       end loop;
    end Fruits_Command;
-
 
    ----------------------------------------------------------
    -- Purpose: Print all the Fruit stats of all Trees in the
    --    Tree_List along with their Fruit count.
    -- Parameters: tl: Tree_List to print
    ----------------------------------------------------------
-   procedure Averages_Command( tl : in Tree_List )
-   is
+   procedure Averages_Command (tl : in Tree_List) is
    begin
       for t in 1 .. tl.count loop
          -- Print the Tree, ID and Fruit count.
-         Put_Tree( tl.trees(t) );
-         Put(":");
+         Put_Tree (tl.trees (t));
+         Put (":");
 
          New_Line;
 
          -- Print the Tree's Fruit stats.
-         Put_Tree_Stats( tl.trees(t).stats );
+         Put_Tree_Stats (tl.trees (t).stats);
          New_Line;
       end loop;
    end Averages_Command;
-
 
 end Commands;
